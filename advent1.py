49f096a1ef76576cb601fdbaff98ca75ae927234
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-02 16:38:22 -0500

    Update advent1.py

--- advent1.py
+++ advent1.py
@@ -1,16 +1,16 @@
 file_in = list(map(int, open('input.txt', 'r').read().split("\n")))
 
 for temp_x in file_in:
-	for temp_y in file_in:
-		if temp_x != temp_y:
-			if temp_x + temp_y == 2020:
-				print(temp_x * temp_y)
+  for temp_y in file_in:
+    if temp_x != temp_y:
+      if temp_x + temp_y == 2020:
+        print(temp_x * temp_y)
 
 for temp_x in file_in:
-	for temp_y in file_in:
-		for temp_z in file_in:
-			if temp_x != temp_y:
-				if temp_y != temp_z:
-					if temp_z != temp_x:
-						if temp_x + temp_y + temp_z == 2020:
-							print(temp_x * temp_y * temp_z)
+  for temp_y in file_in:
+    for temp_z in file_in:
+      if temp_x != temp_y:
+        if temp_y != temp_z:
+          if temp_z != temp_x:
+            if temp_x + temp_y + temp_z == 2020:
+              print(temp_x * temp_y * temp_z)
