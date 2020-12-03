49d40dd28652c0a01c3d9c57ed4a99f1e71cb475
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-03 15:33:04 -0500

    Update advent2.py

--- advent2.py
+++ advent2.py
@@ -1,24 +1,18 @@
 file_in = open('input.txt', 'r').read().replace(":", "").split("\n")
 
-pass_total = len(file_in)
-pass_valid = 0
+def test(input_in): 
+  test_fst, test_sec = 0, 0
+  for temp_test in input_in:
+    temp_split = temp_test.split(" ")
+    temp_min = int(temp_split[0].split("-")[0])
+    temp_max = int(temp_split[0].split("-")[1])
+    temp_fst = int(temp_split[0].split("-")[0]) - 1
+    temp_sec = int(temp_split[0].split("-")[1]) - 1
+    temp_count = temp_split[2].count(temp_split[1])
+    if temp_count >= temp_min and temp_count <= temp_max:
+      test_fst += 1
+    if (temp_split[2][temp_fst] == temp_split[1]) is not (temp_split[2][temp_sec] == temp_split[1]):
+      test_sec += 1
+  return test_fst, test_sec
 
-for temp_test in file_in:
-  temp_split = temp_test.split(" ")
-  temp_min = int(temp_split[0].split("-")[0])
-  temp_max = int(temp_split[0].split("-")[1])
-  temp_count = temp_split[2].count(temp_split[1])
-  if temp_count >= temp_min and temp_count <= temp_max:
-    pass_valid += 1
-
-print(pass_valid)
-pass_valid = 0
-
-for temp_test in file_in:
-  temp_split = temp_test.split(" ")
-  temp_fst = int(temp_split[0].split("-")[0]) - 1
-  temp_sec = int(temp_split[0].split("-")[1]) - 1
-  if (temp_split[2][temp_fst] == temp_split[1]) is not (temp_split[2][temp_sec] == temp_split[1]):
-    pass_valid += 1
-
-print(pass_valid)
+print(test(file_in))
