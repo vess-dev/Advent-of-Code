181c0ab042aff947c56152f20a62929467b271eb
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-03 11:12:02 -0500

    Update advent3.py

--- advent3.py
+++ advent3.py
@@ -1,6 +1,6 @@
 file_in = open("input.txt", "r").read().split("\n")
 
-def slide(map_curr, slide_right, slide_left):
+def slide(map_curr, slide_right, slide_down):
   pos_x = 0
   pos_y = 0
   tree_count = 0
@@ -10,11 +10,11 @@ def slide(map_curr, slide_right, slide_left):
     if map_curr[pos_y][pos_x] == "#":
       tree_count += 1
     pos_x += slide_right
-    pos_y += slide_left
+    pos_y += slide_down
     if pos_y >= map_length:
       return tree_count
     if pos_x >= map_width:
-      pos_x = pos_x % map_width
+      pos_x %= map_width
   return tree_count
 
 print(slide(file_in, 3, 1))
@@ -22,7 +22,7 @@ print(slide(file_in, 3, 1))
 slope_list = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 total_count = 1
 
-for temp_test in slope_list:
-  total_count = total_count * slide(file_in, temp_test[0], temp_test[1])
+for slope_right, slope_down in slope_list:
+  total_count *= slide(file_in, slope_right, slope_down)
 
 print(total_count)
