395c02cb607759e54263eb2a5f8cd5ee9a01ed67
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-06 00:34:46 -0500

    Advent 6

--- advent.py
+++ advent.py
@@ -7,27 +7,28 @@ import advent2
 import advent3
 import advent4
 import advent5
+import advent6
 
-def clock(advent_day, time_tests):
+def clock(advent_day, test_count):
     time_total = 0
-    for temp_step in range(time_tests):
+    for temp_step in range(test_count):
         time_before = time.time()
         advent_day.run()
         time_total += time.time() - time_before
     return time_total
 
-def run(advent_list, time_tests):
+def run(advent_list, test_count):
     time_total = 0
     day_num = 1
     for temp_py in advent_list:
         print("Day", day_num, ":", temp_py.run())
-        time_next = clock(temp_py, time_tests)
-        print(time_tests, "trials of day", day_num, ":", time_next / time_tests, "\n")
+        time_next = clock(temp_py, test_count)
+        print(test_count, "trials of day", day_num, ":", time_next / test_count, "\n")
         time_total += time_next
         day_num += 1
-    print(time_tests, "trials of all :", time_total / time_tests, "\n")
+    print(test_count, "trials of all :", time_total / test_count, "\n")
 
 if __name__ == "__main__":
-    advent_list = [advent1, advent2, advent3, advent4, advent5]
-    time_tests = 20
-    advent.run(advent_list, time_tests)
+    advent_list = [advent1, advent2, advent3, advent4, advent5, advent6]
+    test_count = 20
+    advent.run(advent_list, test_count)
---
+++ advent6.py
@@ -0,0 +1,27 @@
+file_load = open("input6.txt", "r")
+file_in = file_load.read().split("\n\n")
+file_load.close() 
+
+def run():
+
+    def yes(input_in):
+        group_ans = [set(temp_group.replace("\n","")) for temp_group in input_in]
+        total_ans = 0
+        for temp_group in group_ans:
+            total_ans += len(temp_group)
+        return total_ans
+
+    def only(input_in):
+        group_ans = [temp_group.split("\n") for temp_group in input_in]
+        group_final = []
+        for temp_group in group_ans:
+            group_new = []
+            for temp_person in temp_group:
+                group_new.append(set(list(temp_person)))
+            group_final.append(group_new[0].intersection(*group_new))
+        total_ans = 0
+        for temp_group in group_final:
+            total_ans += len(temp_group)
+        return total_ans
+
+    return yes(file_in), only(file_in)
