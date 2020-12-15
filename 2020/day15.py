29e6366ae2acab86b0813ceecbb1e8e72df89776
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-15 03:48:21 -0500

    Update day15.py

--- 2020/day15.py
+++ 2020/day15.py
@@ -7,30 +7,20 @@ file_in = list(map(int, file_in.split(",")))
 
 def run():
 
-	def add(num_last, num_new, game_round):
-		if not num_new in num_last:
-			num_last[num_new] = [game_round, game_round]
-		else:
-			num_last[num_new][1] = num_last[num_new][0]
-			num_last[num_new][0] = game_round
-		return num_last
-
 	def game(input_in, round_hunt):
 		num_last = {}
 		for temp_pos, temp_num in enumerate(input_in, 1):
-			num_last[temp_num] = [temp_pos, temp_pos]
+			num_last[temp_num] = temp_pos
 		game_round = len(input_in)
-		game_last = input_in[-1]
-		while True:
-			game_round += 1
-			if game_last in num_last:
-				num_new = num_last[game_last][0] - num_last[game_last][1]
-				num_last = add(num_last, num_new, game_round)
-				game_last = num_new
+		game_next = 0
+		for game_round in range(len(input_in) + 1, round_hunt):
+			if game_next not in num_last:
+				num_last[game_next] = game_round
+				game_next = 0
 			else:
-				num_last = add(num_last, 0, game_round)
-				game_last = num_new
-			if game_round == round_hunt:
-				return game_last
+				temp_next = game_round - num_last[game_next]
+				num_last[game_next] = game_round
+				game_next = temp_next
+		return game_next
 
 	return game(file_in, 2020), game(file_in, 30000000)
