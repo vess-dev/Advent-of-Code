3d7812081af6ad42e84c2d1f47a41a568332a1a3
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-17 03:51:53 -0500

    Day 17 Update

--- 2020/day17.py
+++ 2020/day17.py
@@ -16,14 +16,17 @@ for temp_ypos, temp_y in enumerate(file_prep):
 
 def run():
 
-	def neigh(input_in, cord_tup):
+	def neigh(input_in, input_dim, cord_tup):
 		close_arr = []
 		close_total = 0
 		for temp_x in range(-1,2):
 			for temp_y in range(-1,2):
 				for temp_z in range(-1,2):
-					for temp_w in range(-1,2):
-						cord_test = (temp_x + cord_tup[0], temp_y + cord_tup[1], temp_z + cord_tup[2], temp_w + cord_tup[3])
+					for temp_w in range(-1, input_dim):
+						if input_dim != 0:
+							cord_test = (temp_x + cord_tup[0], temp_y + cord_tup[1], temp_z + cord_tup[2], temp_w + cord_tup[3])
+						elif input_dim == 0:
+							cord_test = (temp_x + cord_tup[0], temp_y + cord_tup[1], temp_z + cord_tup[2], 0)
 						if cord_test != cord_tup:
 							if cord_test not in input_in:
 								close_arr.append(cord_test)
@@ -31,20 +34,24 @@ def run():
 								close_total += 1
 		return close_arr, close_total
 
-	def cycle(input_in):
+	def cycle(input_in, input_dim):
 		input_new = set()
 		for temp_check in input_in:
-			for temp_neigh in neigh(input_in, temp_check)[0]:
-				if neigh(input_in, temp_neigh)[1] == 3:
+			for temp_neigh in neigh(input_in, input_dim, temp_check)[0]:
+				if neigh(input_in, input_dim, temp_neigh)[1] == 3:
 					input_new.add(temp_neigh)
-			if neigh(input_in, temp_check)[1] in [2, 3]:
+			if neigh(input_in, input_dim, temp_check)[1] in [2, 3]:
 				input_new.add(temp_check) 
 		return input_new
 
 	def six(input_in):
-		input_new = copy.deepcopy(input_in)
+		input_one = copy.deepcopy(input_in)
+		input_two = copy.deepcopy(input_in)
 		for temp_rep in range(6):
-			input_new = cycle(input_new)
-		return len(input_new)
+			input_one = cycle(input_one, 0)
+			input_two = cycle(input_two, 2)
+		return len(input_one), len(input_two)
 
 	return six(file_in)
+
+print(run())
