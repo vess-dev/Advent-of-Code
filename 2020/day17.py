459c6bfa11a476421484baacdaccbc8c4a719d89
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-17 03:29:11 -0500

    Day 17 Part 2

--- 2020/day17.py
+++ 2020/day17.py
@@ -3,73 +3,48 @@ day_num = 17
 import copy
 
 file_load = open("input/day17.txt", "r")
-file_in = file_load.read()
+file_prep = file_load.read()
 file_load.close()
 
-file_len = len(file_in.split("\n")[0]) + 14
-file_in = [[list("." * 7) + list(temp_itr) + list("." * 7) for temp_itr in file_in.split("\n")]]
+file_in = set()
 
-while len(file_in[0]) != file_len:
-	file_in[0].insert(0, (["."] * file_len))
-	file_in[0].insert(len(file_in[0]), (["."] * file_len))
-
-while len(file_in) <= file_len:
-	file_in.insert(0, [["."] * file_len for _ in range(file_len)])
-	file_in.insert(len(file_in), [["."] * file_len for _ in range(file_len)])
+file_prep = [list(temp_itr) for temp_itr in file_prep.split("\n")]
+for temp_ypos, temp_y in enumerate(file_prep):
+	for temp_xpos, temp_x in enumerate(temp_y):
+		if temp_x == "#":
+			file_in.add((temp_xpos, temp_ypos, 0, 0))
 
 def run():
 
-	def peek(input_in, cord_z):
-		for temp_y in input_in[cord_z]:
-			print("".join(temp_y))
-		return
-
-	def alive(input_in):
-		alive_total = 0
-		input_len = len(input_in[0]) - 1
-		for temp_z in range(input_len):
-			for temp_y in range(input_len):
-				for temp_x in range(input_len):
-					if input_in[temp_z][temp_y][temp_x] == "#":
-						alive_total += 1
-		return alive_total
-
-	def close(input_in, cord_x, cord_y, cord_z):
+	def neigh(input_in, cord_tup):
+		close_arr = []
 		close_total = 0
-		close_arr = [(-1, 1),(0,1),(1,1),(-1,0),(0,0),(1,0),(-1,-1),(0,-1),(1,-1)]
-		for temp_z in range(-1,2):
-			for temp_mod in close_arr:
-				if temp_z == 0 and temp_mod == (0,0):
-					pass
-				else:
-					if input_in[cord_z + temp_z][cord_y + temp_mod[1]][cord_x + temp_mod[0]] == "#":
-						close_total += 1
-		return close_total
+		for temp_x in range(-1,2):
+			for temp_y in range(-1,2):
+				for temp_z in range(-1,2):
+					for temp_w in range(-1,2):
+						cord_test = (temp_x + cord_tup[0], temp_y + cord_tup[1], temp_z + cord_tup[2], temp_w + cord_tup[3])
+						if cord_test != cord_tup:
+							if cord_test not in input_in:
+								close_arr.append(cord_test)
+							if cord_test in input_in:
+								close_total += 1
+		return close_arr, close_total
 
 	def cycle(input_in):
-		input_next = copy.deepcopy(input_in)
-		input_len = len(input_in[0]) - 1
-		for temp_x in range(1, input_len):
-			for temp_y in range(1, input_len):
-				for temp_z in range(1, input_len):
-					if input_in[temp_z][temp_y][temp_x] == ".":
-						if close(input_in, temp_x, temp_y, temp_z) == 3:
-							input_next[temp_z][temp_y][temp_x] = "#"
-					elif input_in[temp_z][temp_y][temp_x] == "#":
-						close_count = close(input_in, temp_x, temp_y, temp_z)
-						if close_count not in [2, 3]:
-							input_next[temp_z][temp_y][temp_x] = "."
-						else:
-							input_next[temp_z][temp_y][temp_x] = "#"
-		return input_next
+		input_new = set()
+		for temp_check in input_in:
+			for temp_neigh in neigh(input_in, temp_check)[0]:
+				if neigh(input_in, temp_neigh)[1] == 3:
+					input_new.add(temp_neigh)
+			if neigh(input_in, temp_check)[1] in [2, 3]:
+				input_new.add(temp_check) 
+		return input_new
 
 	def six(input_in):
 		input_new = copy.deepcopy(input_in)
 		for temp_rep in range(6):
-			print(temp_rep + 1)
 			input_new = cycle(input_new)
-		return alive(input_new)
+		return len(input_new)
 
 	return six(file_in)
-
-print(run())
