c6cc405df816c086a7a93e8471eacb9b9eb9cd01
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-18 01:33:27 -0500

    Update day18.py

--- 2020/day18.py
+++ 2020/day18.py
@@ -12,42 +12,29 @@ for temp_pos, temp_line in enumerate(file_in):
 
 def run():
 
-	def opdo(input_one, input_op, input_two):
-		if input_op == "+":
-			return input_one + input_two
-		elif input_op == "-":
-			return input_one - input_two
-		elif input_op == "*":
-			return input_one * input_two
+	def opdo(input_in):
+		if input_in[1] == "+": return input_in[0] + input_in[2]
+		elif input_in[1] == "-": return input_in[0] - input_in[2]
+		elif input_in[1] == "*": return input_in[0] * input_in[2]
 
 	def comp(input_in, input_ord):
 		while len(input_in) != 1:
-			if input_ord:
-				if "+" in input_in:
-					plus_pos = input_in.index("+")
-					input_in[plus_pos - 1] = opdo(input_in[plus_pos - 1], input_in[plus_pos], input_in[plus_pos + 1])
-					input_in.pop(plus_pos)
-					input_in.pop(plus_pos)
-				elif "+" not in input_in:
-					input_ord = False
-			elif not input_ord:
-				input_in[0] = opdo(input_in[0], input_in[1], input_in[2])
-				input_in.pop(1)
-				input_in.pop(1)
+			plus_pos = 1
+			if input_ord and "+" in input_in:
+				plus_pos = input_in.index("+")
+			input_in[plus_pos - 1] = opdo(input_in[plus_pos - 1:plus_pos + 2])
+			input_in.pop(plus_pos)
+			input_in.pop(plus_pos)
 		return input_in[0]
 
 	def maff(input_in, input_ord):
-		input_new = copy.deepcopy(input_in)
-		line_itr = 0
-		stack_maff = []
+		input_new, line_itr = copy.deepcopy(input_in), 0
 		while line_itr != len(input_new):
-			line_curr = input_new[line_itr]
-			line_pos = 0
+			line_curr, line_pos = input_new[line_itr], 0
 			while line_pos != len(line_curr):
 				if "(" in line_curr:
 					temp_stack = []
 					if line_curr[line_pos] == ")":
-						line_par = line_pos
 						while line_curr[line_pos] != "(":
 							line_pos -= 1
 						line_curr.pop(line_pos)
@@ -60,11 +47,9 @@ def run():
 					else:
 						line_pos += 1
 				else:
-					stack_maff.append(comp(line_curr, input_ord))
+					input_new[line_itr] = comp(line_curr, input_ord)
 					break
 			line_itr += 1
-		return sum(stack_maff)
+		return sum(input_new)
 
 	return maff(file_in, False), maff(file_in, True)
-
-print(run())
