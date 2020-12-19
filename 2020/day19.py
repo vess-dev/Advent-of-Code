7a8b827522094ca1eecb33f8b58d5f486062adcc
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-19 02:55:54 -0500

    Update day19.py

--- 2020/day19.py
+++ 2020/day19.py
@@ -1,45 +1,38 @@
 day_num = 19
 
 import copy
-import re
+from collections import defaultdict
 
 file_load = open("input/day19.txt", "r")
-file_in = file_load.read()
+file_prep = file_load.read()
 file_load.close()
 
-file_in = [temp_itr.strip('\n') for temp_itr in file_in.split("\n\n")]
-file_in[0] = dict([temp_itr.split(': ', 1) for temp_itr in file_in[0].split("\n")])
-file_in[1] = file_in[1].split("\n")
+file_in = [defaultdict(list), []]
+file_prep = [temp_itr.strip('\n') for temp_itr in file_prep.split("\n\n")]
+for temp_rule in file_prep[0].replace("\"", "").split("\n"):
+	rule_num, rule_parts = temp_rule.split(": ")
+	for temp_part in rule_parts.split(" | "):
+		if temp_part in ["a", "b"]:
+			file_in[0][int(rule_num)] = temp_part
+		else:
+			file_in[0][int(rule_num)].append(list(eval(temp_itr) for temp_itr in temp_part.split()))
+file_in[1] = file_prep[1].split("\n")
 
 def run():
 
-	def build(input_in, input_num, input_fix):
-		if input_fix:
-			if input_num == "8":
-				return build(input_in, "42", input_fix) + "+"
-			elif input_num == "11":
-				rule_a = build(input_in, "42", input_fix)
-				rule_b = build(input_in, "31", input_fix)
-				return "(?:" + "|".join(f"{rule_a}{{{temp_itr}}}{rule_b}{{{temp_itr}}}" for temp_itr in range(1, 10)) + ")"
-		rule_test = input_in[0][input_num]
-		if re.fullmatch("\".\"", rule_test):
-			return rule_test[1]
+	def itr(input_in, input_rul, input_str):
+		if not input_rul:
+			return len(input_str) == 0
+		rule_check = input_in[0][input_rul.pop(0)]
+		if rule_check in ["a", "b"]:
+			return input_str.startswith(rule_check) and itr(input_in, input_rul, input_str[1:])
 		else:
-			rule_split = rule_test.split(" | ")
-			rule_final = []
-			for temp_rule in rule_split:
-				rule_nums = temp_rule.split(" ")
-				rule_final.append("".join(build(input_in, rule_num, input_fix) for rule_num in rule_nums))
-			return "(?:" + "|".join(rule_final) + ")"
+			return any(itr(input_in, temp_rule + input_rul, input_str) for temp_rule in rule_check)
 
-	def regi(input_in, input_fix):
-		input_new = copy.deepcopy(input_in)
-		regex_str = build(input_new, "0", input_fix)
-		if input_fix:
-			print(regex_str)
-		regex_total = 0
-		for temp_test in input_new[1]:
-			regex_total += bool(re.fullmatch(regex_str, temp_test))
-		return regex_total
+	def rule(input_in, input_adjust):
+		if input_adjust:
+			input_in[0][8] = [[42], [42, 8]]
+			input_in[0][11] = [[42, 31], [42, 11, 31]]
+		return sum(itr(input_in, [0], temp_line) for temp_line in input_in[1])
 
-	return regi(file_in, False), regi(file_in, True)
+	return rule(file_in, False), rule(file_in, True)
