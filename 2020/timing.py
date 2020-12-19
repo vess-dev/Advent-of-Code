b684d202602fab18012199db609e7d464e59bfd9
Vess <33204181+VessToska@users.noreply.github.com>
2020-12-19 05:38:23 -0500

    Update timing.py

--- 2020/timing.py
+++ 2020/timing.py
@@ -32,7 +32,6 @@ def clock(advent_day, test_count):
 
 def run():
     advent_list = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19]
-    advent_list = [day11]
     test_count = 3
     time_total = 0
     if test_count:
