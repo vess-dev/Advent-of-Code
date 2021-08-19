1f1084ce327f3dfb93da271bb6ab9ef19c3fcab7
Vess <33204181+vess-dev@users.noreply.github.com>
2021-08-19 13:11:11 -0400

    working on day5

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -6,15 +6,20 @@ class Comp:
 	op_skip = {
 		"01": 4,
 		"02": 4,
+		"03": 2,
+		"04": 2,
 	}
 
 	def load(self, input_tape):
 		self.mem_tape = input_tape
+		self.mem_pos = 0
 		return
 
 	def get(self, input_pos, input_mode):
 		if input_mode == "0":
 			return self.mem_tape[self.mem_tape[self.mem_pos + input_pos]]
+		elif input_mode == "1":
+			return self.mem_tape[self.mem_pos + input_pos]
 			
 	def next(self):
 		op_next = str(self.mem_tape[self.mem_pos]).rjust(5, "0")
