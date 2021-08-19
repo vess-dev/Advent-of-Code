0af737e403454e8784e3abb70bdb1a6880ef8520
Vess <33204181+vess-dev@users.noreply.github.com>
2021-08-19 03:46:20 -0400

    day 2 clean

---
+++ 2019/day7.py
@@ -0,0 +1,15 @@
+import itertools
+
+day_num = 7
+
+file_load = open("input/day7.txt", "r")
+file_in = file_load.read()
+file_load.close()
+file_in = list(map(int, file_in.split(",")))
+
+def run():
+
+	return
+
+if __name__ == "__main__":
+	print(run())
---
+++ 2019/intcode.py
@@ -0,0 +1,29 @@
+class Comp:
+
+	mem_tape = []
+	mem_pos = 0
+
+	op_skip = {
+		"01": 4,
+		"02": 4,
+	}
+
+	def load(self, input_tape):
+		self.mem_tape = input_tape
+		return
+
+	def get(self, input_pos, input_mode):
+		if input_mode == "0":
+			return self.mem_tape[self.mem_tape[self.mem_pos + input_pos]]
+			
+	def next(self):
+		op_next = str(self.mem_tape[self.mem_pos]).rjust(5, "0")
+		op_next = [op_next[-2:], list(op_next[:3][::-1])]
+		if op_next[0] == "01":
+			self.mem_tape[self.mem_tape[self.mem_pos + 3]] = self.get(1, op_next[1][1]) + self.get(2, op_next[1][2])
+		elif op_next[0] == "02":
+			self.mem_tape[self.mem_tape[self.mem_pos + 3]] = self.get(1, op_next[1][1]) * self.get(2, op_next[1][2])
+		elif op_next[0] == "99":
+			return "Break"
+		self.mem_pos += self.op_skip[op_next[0]]
+		return
