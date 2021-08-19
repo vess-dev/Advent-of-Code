d6c45fc8d73e2fe09389f0b47e3b5aef9ff0b90b
Vess <33204181+vess-dev@users.noreply.github.com>
2021-08-19 03:46:20 -0400

    day 2 clean

--- 2019/day2.py
+++ 2019/day2.py
@@ -1,3 +1,5 @@
+import intcode
+
 day_num = 2
 
 file_load = open("input/day2.txt", "r")
@@ -7,24 +9,15 @@ file_in = list(map(int, file_in.split(",")))
 
 def run():
 
-	def comp(input_in):
-		mem_tape = input_in.copy()
-		mem_pos = 0
-		while True:
-			if mem_tape[mem_pos] == 1:
-				mem_tape[mem_tape[mem_pos + 3]] = mem_tape[mem_tape[mem_pos + 1]] + mem_tape[mem_tape[mem_pos + 2]]
-			elif mem_tape[mem_pos] == 2:
-				mem_tape[mem_tape[mem_pos + 3]] = mem_tape[mem_tape[mem_pos + 1]] * mem_tape[mem_tape[mem_pos + 2]]
-			elif mem_tape[mem_pos] == 99:
-				break
-			mem_pos += 4
-		return mem_tape
-
 	def process(input_in):
 		mem_tape = input_in.copy()
 		mem_tape[1] = 12
 		mem_tape[2] = 2
-		return comp(mem_tape)[0]
+		obj_comp = intcode.Comp()
+		obj_comp.load(mem_tape)
+		while not obj_comp.next():
+			continue
+		return obj_comp.mem_tape[0]
 
 	def find(input_in):
 		for temp_noun in range(100):
@@ -32,7 +25,11 @@ def run():
 				mem_tape = input_in.copy()
 				mem_tape[1] = temp_noun
 				mem_tape[2] = temp_verb
-				if comp(mem_tape)[0] == 19690720:
+				obj_comp = intcode.Comp()
+				obj_comp.load(mem_tape)
+				while not obj_comp.next():
+					continue
+				if obj_comp.mem_tape[0] == 19690720:
 					return (100 * temp_noun) + temp_verb
 
 	return process(file_in), find(file_in)
