52a571c94832383b6a8c18981344fd287c1dced6
Vess <vess.contact@protonmail.com>
2021-11-30 20:18:07 -0500

    attempt

---
+++ 2021/src/day1.rs
@@ -0,0 +1,11 @@
+fn part1() {
+
+}
+
+fn part2() {
+
+}
+
+pub fn main() -> Box<(u32, u32)> {
+	return Box::new((3, 4));
+}
---
+++ 2021/src/day2.rs
@@ -0,0 +1,11 @@
+fn part1() {
+
+}
+
+fn part2() {
+
+}
+
+pub fn main() -> Box<(String, u32)> {
+	return Box::new((String::from("meow"), 4));
+}
