35a3297cc2c4f5296bbad905f7dcb5729f31a049
Vess <vess.contact@protonmail.com>
2021-12-01 02:52:24 -0500

    timing fixed

--- 2021/src/timing.rs
+++ 2021/src/timing.rs
@@ -8,32 +8,29 @@ mod read;
 mod day1;
 mod day2;
 
-enum FnSig {
-    U32U32(fn() -> (u16, u16)),
-    STRU32(fn() -> (String, u32)),
-}
-
-fn run<T1, T2>(func_sig: &FnSig) -> (T1, T2) {
-    if let FnSig::U32U32(func_ref) = func_sig {
-        return func_ref();
-    } else if let FnSig::STRU32(func_ref) = func_sig {
-        return func_ref();
-    } else {
-        return (0, 0)
-    };
+enum FnSig<D1, D2> {
+    U16U16(fn() -> D1),
+    STRU32(fn() -> D2),
 }
 
 fn main() {
-    let vec_days = vec![FnSig::U32U32(day1::main), FnSig::STRU32(day2::main)];
-    let test_count = 3;
+    use FnSig::*;
+    let vec_days = vec![FnSig::U16U16(day1::main), FnSig::STRU32(day2::main)];
+    let test_count = 100;
+    let mut test_ret = String::new();
     let time_total = Instant::now();
     for itr_day in vec_days.iter().enumerate() {
         let time_now = Instant::now();
-        for _temp_test in 1..test_count {
-            run(itr_day.1);
+        for _temp_test in 0..test_count {
+            test_ret = match itr_day.1 {
+                U16U16(func_ref) => format!("{:?}", func_ref()),
+                STRU32(func_ref) => format!("{:?}", func_ref()),
+            };
         }
-        let test_ret = run(itr_day.1);
-        println!("Day {}: {:?}", (itr_day.0)+1, test_ret.1);
-        println!("{} trials of day {}: {}", test_count, itr_day.0, time_now.elapsed().as_secs_f64());
+        let time_elapsed = time_now.elapsed().as_secs_f64();
+        println!("Day {}: {}", (itr_day.0)+1, test_ret);
+        println!("{} trials of day {}: {:.5}", test_count, itr_day.0, (time_elapsed/test_count as f64));
     }
+    let time_elapsed = time_total.elapsed().as_secs_f64();
+    println!("{} trials of all, averages: {:.5}", test_count, (time_elapsed/test_count as f64));
 }
