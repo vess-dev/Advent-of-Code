06652ffdb3ce8b80311a458f40af5d99e12ad2fa
Vess <vess.contact@protonmail.com>
2021-12-02 01:50:06 -0500

    small clean

--- 2021/src/timing.rs
+++ 2021/src/timing.rs
@@ -17,7 +17,7 @@ enum FnSig<D1, D2, DN> {
 fn main() {
     use FnSig::*;
     let vec_days = vec![FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::NULNUL(day3::main)];
-    let test_count = 1000;
+    let test_count = 3;
     let mut test_ret = String::new();
     let time_total = Instant::now();
     for itr_day in vec_days.iter().enumerate() {
@@ -32,8 +32,8 @@ fn main() {
         let time_elapsed = time_now.elapsed().as_secs_f64();
         let time_day = (itr_day.0)+1;
         println!("Day {}: {}", time_day, test_ret);
-        println!("{} trials of day {}: {:.5}s\n", test_count, time_day, (time_elapsed/test_count as f64));
+        println!("{} trials of day {}: {:.6}s\n", test_count, time_day, (time_elapsed/test_count as f64));
     }
     let time_elapsed = time_total.elapsed().as_secs_f64();
-    println!("{} trials of all, averages: {:.5} seconds.", test_count, (time_elapsed/test_count as f64));
+    println!("{} trials of all, averages: {:.6} seconds.", test_count, (time_elapsed/test_count as f64));
 }
