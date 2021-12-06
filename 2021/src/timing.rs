4ffebf37e0d4b6c659b1ae53ff6be55280d42cf3
Vess <vess.contact@protonmail.com>
2021-12-06 01:30:30 -0500

    day 6
    note: caching affects day 6 timing; WAY too fast at iterations > 1

--- 2021/src/timing.rs
+++ 2021/src/timing.rs
@@ -12,17 +12,19 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
-enum FnSig<T1, T2, T3> {
+enum FnSig<T1, T2, T3, T4> {
     U16U16(fn() -> T1),
     U32U32(fn() -> T2),
-    USZUSZ(fn() -> T3),
+    U64U64(fn() -> T3),
+    USZUSZ(fn() -> T4),
 }
 
 fn time() {
    use FnSig::*;
-    let vec_days = [FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::U32U32(day3::main), FnSig::U32U32(day4::main), FnSig::USZUSZ(day5::main)];
-    let test_count = 100;
+    let vec_days = [FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::U32U32(day3::main), FnSig::U32U32(day4::main), FnSig::USZUSZ(day5::main), FnSig::U64U64(day6::main)];
+    let test_count = 3;
     let mut test_ret = String::new();
     let time_total = Instant::now();
     for itr_day in vec_days.iter().enumerate() {
@@ -31,21 +33,22 @@ fn time() {
             test_ret = match itr_day.1 {
                 U16U16(func_ref) => format!("{:?}", func_ref()),
                 U32U32(func_ref) => format!("{:?}", func_ref()),
+                U64U64(func_ref) => format!("{:?}", func_ref()),
                 USZUSZ(func_ref) => format!("{:?}", func_ref()),
             };
         }
         let time_elapsed = time_now.elapsed().as_secs_f64();
         let time_day = (itr_day.0)+1;
         println!("Day {}: {}", time_day, test_ret);
-        println!("{} trials of day {}: {:.6}s\n", test_count, time_day, (time_elapsed/test_count as f64));
+        println!("{} trials of day {}: {:.7}s\n", test_count, time_day, (time_elapsed/test_count as f64));
     }
     let time_elapsed = time_total.elapsed().as_secs_f64();
-    println!("{} trials of all, averages: {:.6} seconds.", test_count, (time_elapsed/test_count as f64)); 
+    println!("{} trials of all, averages: {:.7} seconds.", test_count, (time_elapsed/test_count as f64)); 
 }
 
 fn main() {
     #[cfg(not(debug_assertions))]
     time();
     #[cfg(debug_assertions)]
-    println!("{:?}", day6::main());
+    println!("{:?}", day7::main());
 }
