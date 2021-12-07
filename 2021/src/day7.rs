2bc8635cc6c872200756f559bb3c14c46981389f
Vess <vess.contact@protonmail.com>
2021-12-07 00:57:19 -0500

    day 7
    meme() is a funny one liner

--- 2021/src/day7.rs
+++ 2021/src/day7.rs
@@ -1,18 +1,43 @@
 use crate::read;
 
-fn clean(file_data: String) -> () {
-	return ();
+fn clean(file_data: String) -> Vec<i32> {
+	return file_data.split(",").map(|temp_num| temp_num.parse().unwrap()).collect();
 }
 
-fn part1(data_clean: &()) -> () {
-	return ();
+fn burn(pos_start: i32, pos_goal: i32) -> i32 {
+	return (1..=(pos_start - pos_goal).abs()).sum();
 }
 
-fn part2(data_clean: &()) -> () {
-	return ();
+fn walk(data_clean: &Vec<i32>, flag_burn: bool) -> i32 {
+	let mut vec_fuel: Vec<i32> = Vec::with_capacity(data_clean.len());
+	let vec_min = *data_clean.iter().min().unwrap();
+	let vec_max = *data_clean.iter().max().unwrap();
+	for itr_spot in vec_min..=vec_max {
+		let mut fuel_new: i32 = 0;
+		if flag_burn {
+			fuel_new = data_clean.iter().map(|temp_num| burn(*temp_num, itr_spot)).sum();
+		} else {
+			fuel_new = data_clean.iter().map(|temp_num| (temp_num - itr_spot).abs()).sum();
+		}
+		vec_fuel.push(fuel_new);
+	}
+	return *vec_fuel.iter().min().unwrap();
 }
 
-pub fn main() -> ((), ()) {
+// A condensed one line solve for day 7. Faster than non-meme solution, probably because a lack of a vector.
+fn meme(data_clean: &Vec<i32>, flag_burn: bool) -> i32 {
+	return (*data_clean.iter().min().unwrap()..=*data_clean.iter().max().unwrap()).map(|itr_spot| data_clean.iter().map(|temp_num| if flag_burn {(1..=(temp_num - itr_spot).abs()).sum()} else {(temp_num-itr_spot).abs()}).sum()).fold(i32::MAX, |a, b| a.min(b));
+}
+
+fn part1(data_clean: &Vec<i32>) -> i32 {
+	return walk(data_clean, false);
+}
+
+fn part2(data_clean: &Vec<i32>) -> i32 {
+	return walk(data_clean, true);
+}
+
+pub fn main() -> (i32, i32) {
 	let file_raw = read::as_string("day7.txt");
 	let file_data = clean(file_raw);
 	return (part1(&file_data), part2(&file_data));
--- 2021/src/timing.rs
+++ 2021/src/timing.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unreachable_code)]
-#![allow(unused_variables)]
+#![allow(unused_assignments)]
+#![allow(unused_variables)] 
 
 use std::time::Instant;
 
@@ -13,18 +14,27 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
-enum FnSig<T1, T2, T3, T4> {
-    U16U16(fn() -> T1),
-    U32U32(fn() -> T2),
-    U64U64(fn() -> T3),
-    USZUSZ(fn() -> T4),
+enum FnSig<U16, U32, U64, I16, USZ> {
+    U16U16(fn() -> U16),
+    U32U32(fn() -> U32),
+    U64U64(fn() -> U64),
+    I16I16(fn() -> I16),
+    USZUSZ(fn() -> USZ),
 }
 
 fn time() {
+    let x = i16::MAX;
    use FnSig::*;
-    let vec_days = [FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::U32U32(day3::main), FnSig::U32U32(day4::main), FnSig::USZUSZ(day5::main), FnSig::U64U64(day6::main)];
+    let vec_days = [FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::U32U32(day3::main), FnSig::U32U32(day4::main), FnSig::USZUSZ(day5::main), FnSig::U64U64(day6::main), FnSig::I16I16(day7::main)];
     let test_count = 1;
+    let mut test_type = String::new();
+    if test_count == 1 {
+        test_type = String::from("trial");
+    } else {
+        test_type = String::from("trials");
+    }
     let mut test_ret = String::new();
     let time_total = Instant::now();
     for itr_day in vec_days.iter().enumerate() {
@@ -34,21 +44,22 @@ fn time() {
                 U16U16(func_ref) => format!("{:?}", func_ref()),
                 U32U32(func_ref) => format!("{:?}", func_ref()),
                 U64U64(func_ref) => format!("{:?}", func_ref()),
+                I16I16(func_ref) => format!("{:?}", func_ref()),
                 USZUSZ(func_ref) => format!("{:?}", func_ref()),
             };
         }
         let time_elapsed = time_now.elapsed().as_secs_f64();
         let time_day = (itr_day.0)+1;
         println!("Day {}: {}", time_day, test_ret);
-        println!("{} trials of day {}: {:.7}s\n", test_count, time_day, (time_elapsed/test_count as f64));
+        println!("{} {} of day {}: {:.7}s\n", test_count, test_type, time_day, (time_elapsed/test_count as f64));
     }
     let time_elapsed = time_total.elapsed().as_secs_f64();
-    println!("{} trials of all, averages: {:.7} seconds.", test_count, (time_elapsed/test_count as f64)); 
+    println!("{} {} of all, averages: {:.7} seconds.", test_count, test_type, (time_elapsed/test_count as f64)); 
 }
 
 fn main() {
     #[cfg(not(debug_assertions))]
     time();
     #[cfg(debug_assertions)]
-    println!("{:?}", day6::main());
+    println!("{:?}", day7::main());
 }
