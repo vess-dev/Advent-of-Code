a8dedb0ff9a8d82b62c871858d7e5f332af44b04
Vess <vess.contact@protonmail.com>
2022-11-30 22:56:49 -0500

    2022 Prep

--- 2020/day1.py
+++ 2020/day1.py
@@ -19,11 +19,9 @@ def run():
 		for temp_x in input_in:
 			for temp_y in input_in:
 				for temp_z in input_in:
-					if temp_x != temp_y:
-						if temp_y != temp_z:
-							if temp_z != temp_x:
-								if temp_x + temp_y + temp_z == 2020:
-									return temp_x * temp_y * temp_z
+					if (temp_x != temp_y) and (temp_y != temp_z) and (temp_z != temp_x):
+						if temp_x + temp_y + temp_z == 2020:
+							return temp_x * temp_y * temp_z
 
 	return two(file_in), three(file_in)
 
