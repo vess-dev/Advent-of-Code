66aa0a514d13e95fddb184904fc9b24a8601b365
Vess <vess.contact@protonmail.com>
2022-12-02 00:47:43 -0500

    Day 2

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,13 +1,13 @@
 use crate::read;
 
 fn clean(file_data: &String) -> Vec<Vec<u32>> {
-	return file_data.split("\n\n").map(|temp_x| temp_x.split("\n").map(|temp_y| temp_y.parse::<u32>().unwrap()).collect()).collect();
+	return file_data.split("\n\n").map(|temp_line| temp_line.split("\n").map(|temp_set| temp_set.parse::<u32>().unwrap()).collect()).collect();
 }
 
 fn part1(data_clean: &Vec<Vec<u32>>) -> u32 {
 	let mut sum_max = 0;
-	for itr_x in data_clean {
-		let sum_current = itr_x.iter().sum();
+	for itr_line in data_clean {
+		let sum_current = itr_line.iter().sum();
 		if sum_current > sum_max {
 			sum_max = sum_current;
 		}
@@ -17,8 +17,8 @@ fn part1(data_clean: &Vec<Vec<u32>>) -> u32 {
 
 fn part2(data_clean: &Vec<Vec<u32>>) -> u32 {
 	let mut sum_vec: Vec<u32> = vec!();
-	for itr_x in data_clean {
-		let sum_current = itr_x.iter().sum();
+	for itr_line in data_clean {
+		let sum_current = itr_line.iter().sum();
 		sum_vec.push(sum_current);
 	}
 	sum_vec.sort();
--- 2022/src/timing.rs
+++ 2022/src/timing.rs
@@ -9,44 +9,47 @@ mod read;
 
 mod day1;
 mod day2;
+mod day3;
 
-enum FnSig<U32, NON> {
-    U32U32(fn() -> U32),
-    NONNON(fn() -> NON),
+enum FnSig<U16, U32, NON> {
+	U16U16(fn() -> U16),
+	U32U32(fn() -> U32),
+	NONNON(fn() -> NON),
 }
 
 fn time() {
-    use FnSig::*;
-    let vec_days = [FnSig::U32U32(day1::main), FnSig::NONNON(day2::main)];
-    let test_count = 10;
-    let mut test_type = String::new();
-    if test_count == 1 {
-        test_type = String::from("trial");
-    } else {
-        test_type = String::from("trials");
-    }
-    let mut test_ret = String::new();
-    let time_total = Instant::now();
-    for itr_day in vec_days.iter().enumerate() {
-        let time_now = Instant::now();
-        for _temp_test in 0..test_count {
-            test_ret = match itr_day.1 {
-                U32U32(func_ref) => format!("{:?}", func_ref()),
-                NONNON(func_ref) => format!("{:?}", func_ref()),
-            };
-        }
-        let time_elapsed = time_now.elapsed().as_secs_f64();
-        let time_day = itr_day.0 + 1;
-        println!("Day {}: {}", time_day, test_ret);
-        println!("{} {} of day {}: {:.7}s\n", test_count, test_type, time_day, (time_elapsed/test_count as f64));
-    }
-    let time_elapsed = time_total.elapsed().as_secs_f64();
-    println!("{} {} of all, averages: {:.7} seconds.", test_count, test_type, (time_elapsed/test_count as f64)); 
+	use FnSig::*;
+	let vec_days = [FnSig::U32U32(day1::main), FnSig::U16U16(day2::main), FnSig::NONNON(day3::main)];
+	let test_count = 10;
+	let mut test_type = String::new();
+	if test_count == 1 {
+		test_type = String::from("trial");
+	} else {
+		test_type = String::from("trials");
+	}
+	let mut test_ret = String::new();
+	let time_total = Instant::now();
+	for itr_day in vec_days.iter().enumerate() {
+		let time_now = Instant::now();
+		for _temp_test in 0..test_count {
+			test_ret = match itr_day.1 {
+				U16U16(func_ref) => format!("{:?}", func_ref()),
+				U32U32(func_ref) => format!("{:?}", func_ref()),
+				NONNON(func_ref) => format!("{:?}", func_ref()),
+			};
+		}
+		let time_elapsed = time_now.elapsed().as_secs_f64();
+		let time_day = itr_day.0 + 1;
+		println!("Day {}: {}", time_day, test_ret);
+		println!("{} {} of day {}: {:.7}s\n", test_count, test_type, time_day, (time_elapsed/test_count as f64));
+	}
+	let time_elapsed = time_total.elapsed().as_secs_f64();
+	println!("{} {} of all, averages: {:.7} seconds.", test_count, test_type, (time_elapsed/test_count as f64)); 
 }
 
 fn main() {
-    #[cfg(not(debug_assertions))]
-    time();
-    #[cfg(debug_assertions)]
-    println!("{:?}", day2::main());
+	#[cfg(not(debug_assertions))]
+	time();
+	#[cfg(debug_assertions)]
+	println!("{:?}", day3::main());
 }
