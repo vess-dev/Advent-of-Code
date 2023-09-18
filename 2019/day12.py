c6aa98293ed07de9f3a4a117a2d6619b6f8bc4dd
Vess <vess.contact@protonmail.com>
2023-09-18 08:00:40 -0400

    strat fixes

--- 2019/day12.py
+++ 2019/day12.py
@@ -17,19 +17,19 @@ file_in = [[list(map(int, temp_line.split(" "))), [0, 0, 0]] for temp_line in fi
 def run():
 
 	def timestep(input_list):
-		for temp_moon in input_list:
-			list_other = input_list.copy()
-			list_other.remove(temp_moon)
-			for temp_pull in list_other:
-				for temp_index in range(3):
-					if temp_moon[0][temp_index] < temp_pull[0][temp_index]:
-						temp_moon[1][temp_index] += 1
-					elif temp_moon[0][temp_index] > temp_pull[0][temp_index]:
-						temp_moon[1][temp_index] -= 1
-		for temp_moon in input_list:
+		list_moons = copy.deepcopy(input_list)
+		for temp_moon in list_moons:
+			for temp_pull in list_moons:
+				if temp_moon != temp_pull:
+					for temp_index in range(3):
+						if temp_moon[0][temp_index] < temp_pull[0][temp_index]:
+							temp_moon[1][temp_index] += 1
+						elif temp_moon[0][temp_index] > temp_pull[0][temp_index]:
+							temp_moon[1][temp_index] -= 1
+		for temp_moon in list_moons:
 			for temp_index in range(3):
 				temp_moon[0][temp_index] += temp_moon[1][temp_index]
-		return input_list
+		return list_moons
 
 	def energy(input_list):
 		energy_final = 0
@@ -51,13 +51,12 @@ def run():
 	def smallstep(input_list):
 		list_moons = copy.deepcopy(input_list)
 		for temp_moon in list_moons:
-			list_other = list_moons.copy()
-			list_other.remove(temp_moon)
-			for temp_pull in list_other:
-				if temp_moon[0] < temp_pull[0]:
-					temp_moon[1] += 1
-				elif temp_moon[0] > temp_pull[0]:
-					temp_moon[1] -= 1
+			for temp_pull in list_moons:
+				if temp_moon != temp_pull:
+					if temp_moon[0] < temp_pull[0]:
+						temp_moon[1] += 1
+					elif temp_moon[0] > temp_pull[0]:
+						temp_moon[1] -= 1
 		for temp_moon in list_moons:
 			temp_moon[0] += temp_moon[1]
 		return list_moons
@@ -67,36 +66,32 @@ def run():
 		x_slice = [[temp_moon[0][0], 0] for temp_moon in list_moons]
 		y_slice = [[temp_moon[0][1], 0] for temp_moon in list_moons]
 		z_slice = [[temp_moon[0][2], 0] for temp_moon in list_moons]
-		x_list, x_toggle, x_final = [], True, 0
-		y_list, y_toggle, y_final = [], True, 0
-		z_list, z_toggle, z_final = [], True, 0
-		while x_toggle:
-			if x_toggle:
-				x_slice = smallstep(x_slice)
-				if x_slice in x_list:
-					x_toggle = False
-					x_final = len(x_list)
-					x_list.clear()
-				x_list.append(x_slice)
-		while y_toggle:
-			if y_toggle:
-				y_slice = smallstep(y_slice)
-				if y_slice in y_list:
-					y_toggle = False
-					y_final = len(y_list)
-					y_list.clear()
-				y_list.append(y_slice)
-		while z_toggle:
-			if z_toggle:
-				z_slice = smallstep(z_slice)
-				if z_slice in z_list:
-					z_toggle = False
-					z_final = len(z_list)
-					z_list.clear()
-				z_list.append(z_slice)
+		x_final, y_final, z_final = 0, 0, 0
+		list_history = {}
+		while True:
+			x_slice = smallstep(x_slice)
+			if str(x_slice) in list_history:
+				x_final = len(list_history)
+				list_history.clear()
+				break
+			list_history[str(x_slice)] = True
+		while True:
+			y_slice = smallstep(y_slice)
+			if str(y_slice) in list_history:
+				y_final = len(list_history)
+				list_history.clear()
+				break
+			list_history[str(y_slice)] = True
+		while True:
+			z_slice = smallstep(z_slice)
+			if str(z_slice) in list_history:
+				z_final = len(list_history)
+				list_history.clear()
+				break
+			list_history[str(z_slice)] = True
 		return math.lcm(x_final, y_final, z_final)
 
-	return simulate(file_in)#, history(file_in)
+	return simulate(file_in), history(file_in)
 
 if __name__ == "__main__":
 	print(run())
