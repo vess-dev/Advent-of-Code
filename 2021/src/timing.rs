c6fd5ed85af6f4a53f90b7b371b647ee722c7b27
Vess <vess.contact@protonmail.com>
2023-11-30 10:42:23 -0500

    rough

--- 2021/src/timing.rs
+++ 2021/src/timing.rs
@@ -37,11 +37,9 @@ fn time() {
     use FnSig::*;
     let vec_days = [FnSig::U16U16(day1::main), FnSig::U32U32(day2::main), FnSig::U32U32(day3::main), FnSig::U32U32(day4::main), FnSig::U16U16(day5::main), FnSig::U64U64(day6::main), FnSig::I16I16(day7::main), FnSig::U16U32(day8::main), FnSig::U16U32(day9::main), FnSig::U32U64(day10::main), FnSig::U16U16(day11::main), FnSig::U32U32(day12::main), FnSig::U16NON(day13::main), FnSig::U64U64(day14::main)];
     let test_count = 10;
-    let mut test_type = String::new();
-    if test_count == 1 {
-        test_type = String::from("trial");
-    } else {
-        test_type = String::from("trials");
+    let mut test_type = String::from("trial");
+    if test_count > 1 {
+        test_type.push('s');
     }
     let mut test_ret = String::new();
     let time_total = Instant::now();
