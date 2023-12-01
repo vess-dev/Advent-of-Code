fea227336b671baea9d759731ab83a7db084a4c0
Vess <vess.contact@protonmail.com>
2023-12-01 11:25:10 -0500

    Update day2.go

--- 2023/day2.go
+++ 2023/day2.go
@@ -18,7 +18,7 @@ func d2part2(in_clean any) any {
 }
 
 func day2() (any, any) {
-	file_string := tload("input/day1.txt")
+	file_string := tload("input/day2.txt")
 	file_clean := d2clean(file_string)
 	return d2part1(file_clean), d2part2(file_clean)
 }
