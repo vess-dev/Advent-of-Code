f348d6f6e1e98faa78b0d959264eb9f8c37bcd98
Vess <vess.contact@protonmail.com>
2023-12-02 09:35:12 -0500

    Update day2.go

--- 2023/day2.go
+++ 2023/day2.go
@@ -15,27 +15,25 @@ func d2clean(in_raw string) [][]d2game {
 	string_list := strings.Split(in_raw, "\n")
 	var final_list [][]d2game
 	for _, temp_game := range string_list {
-		game_list := strings.Split(temp_game, ": ")
-		for _, temp_line := range game_list[1:] {
-			draw_list := strings.Split(temp_line, "; ")
-			var draw_stack []d2game
-			for _, temp_draw := range draw_list {
-				hand_list := strings.Split(temp_draw, ", ")
-				game_new := d2game{}
-				for _, temp_hand := range hand_list {
-					grab_list := strings.Split(temp_hand, " ")
-					int_data, int_error := strconv.Atoi(grab_list[0])
-					tcheck(int_error)
-					switch grab_list[1] {
-						case "red": game_new.red = int_data
-						case "green": game_new.green = int_data
-						case "blue": game_new.blue = int_data
-					}
+		game_list := strings.Split(temp_game, ": ")[1]
+		draw_list := strings.Split(game_list, "; ")
+		var draw_stack []d2game
+		for _, temp_draw := range draw_list {
+			hand_list := strings.Split(temp_draw, ", ")
+			game_new := d2game{}
+			for _, temp_hand := range hand_list {
+				grab_list := strings.Split(temp_hand, " ")
+				int_data, int_error := strconv.Atoi(grab_list[0])
+				tcheck(int_error)
+				switch grab_list[1] {
+					case "red": game_new.red = int_data
+					case "green": game_new.green = int_data
+					case "blue": game_new.blue = int_data
 				}
-				draw_stack = append(draw_stack, game_new)
 			}
-			final_list = append(final_list, draw_stack)
+			draw_stack = append(draw_stack, game_new)
 		}
+		final_list = append(final_list, draw_stack)
 	}
 	return final_list
 }
