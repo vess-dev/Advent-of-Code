90797df8c64f6f8e45a1f75cc64030d2e4de8245
Vess <vess.contact@protonmail.com>
2023-12-05 14:54:53 -0500

    small progress

--- 2023/day5.go
+++ 2023/day5.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	
+	"strings"
 )
 
+type d5Range struct {
+
+}
+
 func d5clean(in_raw string) any {
-	out_clean := in_raw
-	return out_clean
+	string_list := strings.Split(in_raw, "\n\n")
+	split_list := make([][]string, len(string_list))
+	for temp_idx, temp_text := range string_list {
+		split_list[temp_idx] = strings.Split(temp_text, "\n")
+	}
+	split_list[0] = strings.Split(split_list[0][0], ": ")
+	for temp_idx, temp_line := range split_list {
+		split_list[temp_idx] = tdrop(temp_line, 0)
+	}
+	ret_input := tcast(strings.Split(split_list[0][0], " "))
+	split_list = tdrop(split_list, 0)
+
+	tprint(split_list)
+	tuse(ret_input)
+	return 5
 }
 
 func d5part1(in_clean any) any {
