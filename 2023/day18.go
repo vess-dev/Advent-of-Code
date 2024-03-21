8baf6fa4892164d1e18e725b6cb6e5068efa8fa5
Vess <vess.contact@protonmail.com>
2024-03-21 16:17:32 +0100

    idk

--- 2023/day18.go
+++ 2023/day18.go
@@ -15,11 +15,95 @@ type d18Point struct {
 }
 type d18Digsite struct {
 	orders []d18Order
-	digsite map[d18Point]bool
+	pointer int
+	digmap map[d18Point]bool
+	digx int
+	digy int
+	maxx int
+	minx int
+	maxy int
+	miny int
+}
+var d18REF = map[string][]int {
+	"U": {0, -1},
+	"R": {1, 0},
+	"D": {0, 1},
+	"L": {-1, 0},
+}
+
+func (self *d18Digsite) size() int {
+	return len(self.digmap)
+}
+
+func (self *d18Digsite) check(in_x int, in_y int) bool {
+	point_check := d18Point{in_x, in_y}
+	return self.digmap[point_check]
+}
+
+func (self *d18Digsite) space() int {
+	var total_space int
+	for temp_y := self.miny; temp_y <= self.maxy; temp_y++ {
+		var toggle_wall bool
+		var toggle_inside bool
+		var count_inside int
+		test_mod := 5
+		for temp_x := self.minx - test_mod; temp_x <= self.maxx + test_mod; temp_x++ {
+			if self.check(temp_x, temp_y) {
+				toggle_wall = true
+			} else {
+				if !toggle_inside {
+					if toggle_wall {
+						toggle_wall = false
+						toggle_inside = true
+						count_inside += 1
+					}
+				} else if toggle_wall && toggle_inside {
+					toggle_wall = false
+					toggle_inside = false
+					total_space += count_inside
+				} else {
+					count_inside += 1
+				}
+			}
+		}
+	}
+	return self.size() + total_space
+}
+
+func (self *d18Digsite) set(in_x int, in_y int) {
+	point_new := d18Point{in_x, in_y}
+	self.digmap[point_new] = true
+	if in_x > self.maxx {
+		self.maxx = in_x
+	}
+	if in_x < self.minx {
+		self.minx = in_x
+	}
+	if in_y > self.maxy {
+		self.maxy = in_y
+	}
+	if in_y < self.miny {
+		self.miny = in_y
+	}
+	return
+}
+
+func (self *d18Digsite) dig() {
+	for self.pointer < len(self.orders) {
+		order_current := self.orders[self.pointer]
+		for temp_rep := 0; temp_rep < order_current.len; temp_rep++ {
+			self.digx += d18REF[order_current.dir][0]
+			self.digy += d18REF[order_current.dir][1]
+			self.set(self.digx, self.digy)
+		}
+		self.pointer += 1
+	}
+	return
 }
 
 func d18clean(in_raw string) d18Digsite {
 	out_digsite := d18Digsite{}
+	out_digsite.digmap = make(map[d18Point]bool)
 	line_split := strings.Split(in_raw, "\n")
 	for _, temp_line := range line_split {
 		order_split := strings.Split(temp_line, " ")
@@ -29,13 +113,25 @@ func d18clean(in_raw string) d18Digsite {
 	return out_digsite
 }
 
-func d18part1(in_clean any) any {
+func d18part1(in_clean d18Digsite) int {
+	tline()
 	tdebug(in_clean)
+	tline()
+	in_clean.dig()
+	tline(in_clean.digmap)
+	tline()
+	tdebug(in_clean)
+	tline()
+	tline(in_clean.size())
+	tline()
+	tline(in_clean.space())
+	tline()
 	return -1
 }
 
-func d18part2(in_clean any) any {
-	tdebug(in_clean)
+func d18part2(in_clean d18Digsite) int {
+	//tdebug(in_clean)
+	tuse(in_clean)
 	return -1
 }
 
