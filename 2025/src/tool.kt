3ee6528f7955c249590769d2418cc9a9c1663e41
Vess <vess.contact@protonmail.com>
2026-03-11 17:29:33 +0100

    Update tool.kt

--- 2025/src/tool.kt
+++ 2025/src/tool.kt
@@ -5,92 +5,94 @@ import kotlin.math.abs
 import kotlin.math.log10
 import kotlin.math.sqrt
 
+typealias Point = Tool.Point
+typealias Grid = Tool.Grid
+
 object Tool {
-	const val PREPEND = "input/"
+    private const val PREPEND = "input/"
 	fun readInput(inName: String): String {
 		val pathFull = "$PREPEND$inName.txt"
 		return File(pathFull).readText()
 	}
-}
 
-class Point(var xPos: Int = 0, var yPos: Int = 0) {
-    fun add(inPoint: Point) {
-        this.xPos += inPoint.xPos
-        this.yPos += inPoint.yPos
+    class Point(var xPos: Int = 0, var yPos: Int = 0) {
+        fun add(inPoint: Point) {
+            this.xPos += inPoint.xPos
+            this.yPos += inPoint.yPos
+        }
+        fun testAdd(inPoint: Point): Point {
+            return Point(this.xPos + inPoint.xPos, this.yPos + inPoint.yPos)
+        }
+        fun incX(inXAdd: Int = 1) {
+            this.xPos += inXAdd
+        }
+        fun incY(inYAdd: Int = 1) {
+            this.yPos += inYAdd
+        }
+        fun decX(inXDec: Int = 1) {
+            this.xPos -= inXDec
+        }
+        fun decY(inYDec: Int = 1) {
+            this.yPos -= inYDec
+        }
+        override fun toString(): String {
+            return "($xPos,$yPos)"
+        }
+        override fun equals(other: Any?): Boolean {
+            if (other !is Point) return false
+            return this.xPos == other.xPos && this.yPos == other.yPos
+        }
+        override fun hashCode(): Int {
+            return Objects.hash(xPos, yPos)
+        }
     }
-    fun testAdd(inPoint: Point): Point {
-        return Point(this.xPos + inPoint.xPos, this.yPos + inPoint.yPos)
-    }
-    fun incX(inXAdd: Int = 1) {
-        this.xPos += inXAdd
-    }
-    fun incY(inYAdd: Int = 1) {
-        this.yPos += inYAdd
-    }
-    fun decX(inXDec: Int = 1) {
-        this.xPos -= inXDec
-    }
-    fun decY(inYDec: Int = 1) {
-        this.yPos -= inYDec
-    }
-    override fun toString(): String {
-        return "($xPos,$yPos)"
-    }
-    override fun equals(other: Any?): Boolean {
-        if (other !is Point) return false
-        return this.xPos == other.xPos && this.yPos == other.yPos
-    }
-    override fun hashCode(): Int {
-        return Objects.hash(xPos, yPos)
-    }
-}
 
-class Grid(val inSize: Int = 0, val grid: HashMap<Point, Char> = HashMap(inSize)) {
-    private val relative = listOf(
-        Point(-1, -1),
-        Point(0, -1),
-        Point(1, -1),
-        Point(-1, 0),
-        Point(1, 0),
-        Point(-1, 1),
-        Point(0, 1),
-        Point(1, 1),
-    )
-    private val sqrt = sqrt(inSize.toDouble()).toInt()
-    fun add(inPoint: Point, inChar: Char) {
-        grid[inPoint] = inChar
+    class Grid(val inSize: Int = 0, val grid: HashMap<Point, Char> = HashMap(inSize)) {
+        private val relative = listOf(
+            Point(-1, -1),
+            Point(0, -1),
+            Point(1, -1),
+            Point(-1, 0),
+            Point(1, 0),
+            Point(-1, 1),
+            Point(0, 1),
+            Point(1, 1),
+        )
+        private val sqrt = sqrt(inSize.toDouble()).toInt()
+        fun add(inPoint: Point, inChar: Char) {
+            grid[inPoint] = inChar
+        }
+        fun remove(inPoint: Point) {
+            grid.remove(inPoint)
+        }
+        fun get(inPoint: Point): Char? {
+            return grid.get(inPoint)
+        }
+        fun getNearby(inPoint: Point): List<Char> {
+            return relative.mapNotNull { offset -> get(inPoint.testAdd(offset)) }
+        }
+        fun keys(): Set<Point> {
+            return grid.keys
+        }
+        fun find(inChar: Char): Set<Point> {
+            return grid.filterValues { char -> char == inChar }.keys
+        }
+        fun within(inPoint: Point): Boolean {
+            return 0 <= inPoint.xPos && 0 <= inPoint.yPos && inPoint.xPos < sqrt && inPoint.yPos < sqrt
+        }
     }
-    fun remove(inPoint: Point) {
-        grid.remove(inPoint)
-    }
-    fun get(inPoint: Point): Char? {
-        return grid.get(inPoint)
-    }
-    fun getNearby(inPoint: Point): List<Char> {
-        return relative.mapNotNull { offset -> get(inPoint.testAdd(offset)) }
-    }
-    fun keys(): Set<Point> {
-        return grid.keys
-    }
-    fun find(inChar: Char): Set<Point> {
-        return grid.filterValues { char -> char == inChar }.keys
-    }
-    fun within(inPoint: Point): Boolean {
-        return 0 <= inPoint.xPos && 0 <= inPoint.yPos && inPoint.xPos < sqrt && inPoint.yPos < sqrt
+
+    private const val PP_INDENT = 2
+    private const val PP_WRAP = 140
+    fun inspect(vararg inObjects: Any?) {
+        for (tempObject in inObjects) {
+            pp(tempObject, indent = PP_INDENT, wrappedLineWidth = PP_WRAP)
+            println()
+        }
     }
 }
 
 fun Int.length() = when(this) {
     0 -> 1
     else -> log10(abs(toDouble())).toInt() + 1
-}
-
-private const val PP_INDENT = 2
-private const val PP_WRAP = 140
-
-fun inspect(vararg inObjects: Any?) {
-    for (tempObject in inObjects) {
-        pp(tempObject, indent = PP_INDENT, wrappedLineWidth = PP_WRAP)
-        println()
-    }
 }
