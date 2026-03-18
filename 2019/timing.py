bbcd56490d551a5eb4767cefa341e342bff2a038
Vess <vess.contact@protonmail.com>
2026-03-18 01:55:44 +0100

    Update timing.py

--- 2019/timing.py
+++ 2019/timing.py
@@ -37,7 +37,7 @@ def clock(advent_day, test_count):
 
 def run():
 	advent_list = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17]
-	test_count = 1
+	test_count = 10
 	time_total = 0
 	if test_count:
 		for temp_py in advent_list:
